dfc9863ba1803d86cfa0238881e4873a0ab7949b
Hugo HOVHANNESSIAN <hugo.hovhannessian@gmail.com>
2022-04-01 12:15:55 +0200

    Feat : modification du main

--- projet_cpp.cpp
+++ projet_cpp.cpp
@@ -10,13 +10,9 @@ int main()
     cout << "J'ai réussi à faire le dépot git";
 }
 
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
+// Quick tips : 
+// - Execute : Ctrl+F5 or menu Déboguer > Exécuter sans débogage
+// - Déboguer : F5 or menu Déboguer > Démarrer le débogage
+// - For the classes, we will put them on "fichiers source"
+// - For the .h files, we will put them on "fichiers d'en-tête"
 
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
