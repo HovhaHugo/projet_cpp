4de2bc6755779442e5394f5cad6f6ada43925e40
Hugo HOVHANNESSIAN <hugo.hovhannessian@etu.univ-tours.fr>
2022-05-02 11:44:05 +0200

    feat : Ajout des modif faites sur CMatrix

--- CMatrix.h
+++ CMatrix.h
@@ -18,21 +18,101 @@ public:
 	unsigned int uiMATColumnNumber;
 	unsigned int uiMATLineNumber;
 	Type** pptypeMatrixContent;
+	//Constructors
+	CMatrix();
+	CMatrix(Type** ptTypeMatrix,unsigned int uiNbColomn, unsigned int uiNbLine);
 	CMatrix(const char* pcFilePath);
+	CMatrix(CMatrix MATParam);
+
+	//Overloaded Operators
+	operator*(Type tMultiplication);
+	operator*(CMatrix MATParam);
+	operator+(CMatrix MATParam);
+	operator-(CMatrix MATParam);
+
+	//Method
+	CMatrix MATTransposer();
+	void MATshow();
+	unsigned int MATGetColumnNumbre();
+	unsigned int MATGetLineNumbre();
+	Type MATGetValue(unsigned int uiColomn, unsigned int uiLine);
+
+	CMatrix(const char* pcFilePath) {
+
+		try {
+			pptypeMatrixContent = ParseDataDouble(pcFilePath, &uiMATColumnNumber, &uiMATLineNumber);
+		}
+		catch (CException& EXCException) {
+			printf("An error occured\nError code: %d\nFunction: %s\n",EXCException.EXCgetValue(),EXCException.EXCgetFunction());
+		}
+
+	}
+
+	CMatrix() {
+		uiMATColumnNumber = 0;
+		uiMATLineNumber = 0;
+		lisMATContent = NULL;
+	}
+
+	CMatrix(Type** ptTypeMatrix, unsigned int uiNbColomn, unsigned int uiNbLine) {
+		uiMATColumnNumber = uiNbColomn;
+		uiMATLineNumber = uiNbLine;
+		for (int iLoopColomne = 0; iLoopColomne <= uiMATColumnNumber; iLoopColomne++) {
+			for (int iLoopLine = 0; iLoopLine <= uiMATLineNumber; iLoopLine++) {
+				lisMATContent[iLoopColomne][iLoopLine] = ptTypeMatrix[iLoopColomne][iLoopLine];
+			}
+		}
+	}
+
+	CMatrix(const char* pcChemin) {
+		uiMATColumnNumber = 0;
+		uiMATLineNumber = 0;
+		lisMATContent = Parser.PARParseData(pcChemin, &uiMATColomneNumber, &uiMATLineNumber);
+	}
+
+	CMatrix(CMatrix MATParam) {
+		uiMATColumnNumber = MATParam.uiMATColumnNumber;
+		uiMATLineNumber = MATParam.uiMATLineNumber;
+		for (int iLoopColomne = 0; iLoopColomne <= uiMATColumnNumber; iLoopColomne++) {
+			for (int iLoopLine = 0; iLoopLine <= uiMATLineNumber; iLoopLine++) {
+				lisMATContent[iLoopColomne][iLoopLine] = MATParam.MATGetValue(iLoopColomne, iLoopLine);
+			}
+		}
+	}
+
+	CMatrix MATTransposer() {
+
+	}
+
+	void MATshow() {
+		for (int iLoopColomne = 0; iLoopColomne <= uiMATColumnNumber; iLoopColomne++) {
+			cout << "(";
+			for (int iLoopLine = 0; iLoopLine <= uiMATLineNumber; iLoopLine++) {
+				cout<<lisMATContent[iLoopColomne][iLoopLine] ;
+			}
+			cout << ")\n";
+		}
+	}
+	
+	unsigned int MATGetColumnNumbre() {
+		return uiMATColumnNumber;
+	}
+
+	unsigned int MATGetLineNumbre() {
+		return uiMATLineNumber;
+	}
+
+	Type MATGetValue(unsigned int uiColomn, unsigned int uiLine) {
+		for (int iLoopColomne = 0; iLoopColomne <= uiMATColumnNumber; iLoopColomne++) {
+			for (int iLoopLine = 0; iLoopLine <= uiMATLineNumber; iLoopLine++) {
+				if (iLoopLine == uiLine && iLoopColomne == uiColomn) {
+					return lisMATContent[iLoopColomne][iLoopLine];
+				}
+			}
+		}
+	}
 
 };
-
-template<class Type>
-CMatrix<Type>::CMatrix(const char* pcFilePath) {
-
-	try {
-		pptypeMatrixContent = ParseDataDouble(pcFilePath, &uiMATColumnNumber, &uiMATLineNumber);
-	}
-	catch (CException& EXCException) {
-		printf("An error occured\nError code: %d\nFunction: %s\n",EXCException.EXCgetValue(),EXCException.EXCgetFunction());
-	}
-
-}
 
 
 #endif
--- projet_cpp.cpp
+++ projet_cpp.cpp
@@ -15,5 +15,47 @@ int main()
 
 	delete matricetest;
 
+	size_t stTaille = 3;
+    int iVal1 = 1;
+    int iVal2 = 2;
+    int iVal3 = 3;
+    int iVal4 = 4;
+    int iVal5 = 5;
+    int iVal6 = 6;
+    int iVal7 = 7;
+    int iVal8 = 8;
+    int iVal9 = 9;
+    //cout << iVal9;
+    int* iLigne1 = new int();
+    iLigne1[0] = iVal1;
+    iLigne1[1] = iVal2;
+    iLigne1[2] = iVal3;
+    int* iLigne2 = new int();
+    iLigne2[0] = iVal4;
+    iLigne2[1] = iVal5;
+    iLigne2[2] = iVal6;
+    int* iLigne3 = new int();
+    iLigne3[0] = iVal7;
+    iLigne3[1] = iVal6;
+    iLigne3[2] = iVal9;
+    int** ptMatrix = new int* [3];
+    ptMatrix[0] = iLigne1;
+    ptMatrix[1] = iLigne2;
+    ptMatrix[2] = iLigne3;
+
+    cout << "Contenue de la matrice : \n";
+    for (int x = 0; x < 3; x++) {
+        cout << "(";
+        for (int y = 0; y < 3; y++) {
+            cout << ptMatrix[x][y];
+            cout << " , ";
+            //*ptMatrix++;
+        }
+        cout << ")\n";
+    }
+
+    CMatrix<int> matrice = CMatrix<int>(ptMatrix,3,3);
+    matrice.MATGetColumnNumbre();
+
 	cout << "test fini" << endl;
 }
